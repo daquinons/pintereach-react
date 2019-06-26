e95ee3f2c5d7c20b1b2219b4589848fe06eff8ee
David Quiñones <daquinons@users.noreply.github.com>
2019-06-27 00:19:44 +0200

    Center BoardsList and ArticlesList

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getAllUserBoards, postArticle, getArticles } from '../../state/reducers/boards';
+import {
+  getAllUserBoards,
+  postArticle,
+  getArticles
+} from '../../state/reducers/boards';
 import withRestrictedToAuth from '../../hoc/withRestrictedToAuth';
 import CardArticle from '../Card/CardArticle';
 import HeaderContainer from '../HeaderContainer/HeaderContainer';
 import ButtonLink from '../ButtonLink/ButtonLink';
 import StyledContainerFlex from '../StyledContainerFlex/StyledContainerFlex';
 import AddArticleForm from '../AddArticleForm/AddArticleForm';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -24,9 +29,9 @@ const ArticlesList = props => {
       // If the user is not the owner of the owner of the board
       setOwner(false);
       const articles = await getArticles(boardId);
-      selected = {board_title: "Shared Board", articles }
+      selected = { board_title: 'Shared Board', articles };
     }
-    
+
     if (selected) {
       setCurrentBoard(selected);
       setArticles(selected.articles);
@@ -51,27 +56,35 @@ const ArticlesList = props => {
         <HeaderContainer>
           <Row>
             <Col sm={4}>{currentBoard.board_title}</Col>
-            {owner ? (<Col className="text-md-right" sm={{ span: 1, offset: 7 }}>
-              <ButtonLink onClick={() => setModalShow(true)}>Add</ButtonLink>
-              <AddArticleForm
-                addArticle={postArticle}
-                boardId={boardId}
-                userId={userId}
-                show={modalShow}
-                onHide={modalClose}
-              />
-            </Col>) : null }
+            {owner ? (
+              <Col className="text-md-right" sm={{ span: 1, offset: 7 }}>
+                <ButtonLink onClick={() => setModalShow(true)}>Add</ButtonLink>
+                <AddArticleForm
+                  addArticle={postArticle}
+                  boardId={boardId}
+                  userId={userId}
+                  show={modalShow}
+                  onHide={modalClose}
+                />
+              </Col>
+            ) : null}
           </Row>
         </HeaderContainer>
-        <StyledContainerFlex>
-          {articles ? (
-            articles.map((article, index) => (
-              <CardArticle key={index} article={article} />
-            ))
-          ) : (
-            <div>No articles, please add one to your board.</div>
-          )}
-        </StyledContainerFlex>
+        <Container>
+          <Row>
+            <Col className="text-center" md={{ span: 12 }}>
+              <StyledContainerFlex>
+                {articles ? (
+                  articles.map((article, index) => (
+                    <CardArticle key={index} article={article} />
+                  ))
+                ) : (
+                  <div>No articles, please add one to your board.</div>
+                )}
+              </StyledContainerFlex>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
@@ -83,7 +96,7 @@ const mapStateToProps = state => {
   return {
     boards: state.boards.boards,
     userId: state.auth.userId,
-    isLoggedIn: !!state.auth.token && state.auth.token !== "null"
+    isLoggedIn: !!state.auth.token && state.auth.token !== 'null'
   };
 };
 
--- src/components/BoardsList/BoardsList.jsx
+++ src/components/BoardsList/BoardsList.jsx
@@ -7,6 +7,7 @@ import HeaderContainer from '../HeaderContainer/HeaderContainer';
 import AddBoardForm from '../AddBoardForm/AddBoardForm';
 import StyledContainerFlex from '../StyledContainerFlex/StyledContainerFlex';
 import CardBoard from '../Card/CardBoard';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -30,23 +31,29 @@ const BoardsList = props => {
           <Col className="text-md-right" sm={{ span: 1, offset: 7 }}>
             <ButtonLink onClick={() => setModalShow(true)}>Add</ButtonLink>
             <AddBoardForm
-                addBoard={postBoard}
-                userId={userId}
-                show={modalShow}
-                onHide={modalClose}
-              />
+              addBoard={postBoard}
+              userId={userId}
+              show={modalShow}
+              onHide={modalClose}
+            />
           </Col>
         </Row>
       </HeaderContainer>
-      <StyledContainerFlex>
-        {props.boards.length > 0 ? (
-          props.boards.map((board, index) => (
-            <CardBoard key={index} board={board} />
-          ))
-        ) : (
-          <div>No boards, please add one.</div>
-        )}
-      </StyledContainerFlex>
+      <Container>
+        <Row>
+          <Col className="text-center" md={{ span: 12 }}>
+            <StyledContainerFlex>
+              {props.boards.length > 0 ? (
+                props.boards.map((board, index) => (
+                  <CardBoard key={index} board={board} />
+                ))
+              ) : (
+                <div>No boards, please add one.</div>
+              )}
+            </StyledContainerFlex>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
