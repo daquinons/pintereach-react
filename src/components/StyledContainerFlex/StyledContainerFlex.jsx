4351a5cf277be0fc9f6d7955bc1d0a41bad207eb
David Quiñones <daquinons@users.noreply.github.com>
2019-06-27 09:36:30 +0200

    Fix responsive position of boards

--- src/components/StyledContainerFlex/StyledContainerFlex.jsx
+++ src/components/StyledContainerFlex/StyledContainerFlex.jsx
@@ -5,6 +5,11 @@ const StyledContainerFlex = styled(Container)`
   display: flex;
   flex-wrap: wrap;
   justify-content: initial;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 export default StyledContainerFlex;
