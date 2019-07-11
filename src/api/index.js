edf26d11ed6d1d52622115b25ab63d5d0f13f61a
David Quiñones <daquinons@users.noreply.github.com>
2019-07-11 13:23:01 +0200

    Update API Url

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://pintereach0.herokuapp.com/api';
+const API_URL = 'https://pintereach-api.herokuapp.com/api';
 //const API_URL = 'http://localhost:4000/api';
 const AUTH_URL = API_URL + '/auth';
 export const LOGIN_URL = AUTH_URL + '/login/';
